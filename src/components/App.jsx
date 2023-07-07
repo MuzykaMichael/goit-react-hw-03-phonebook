1cb2ad5d15f0cbf9886ac7efad038c4ef30f8930
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-07-07 20:48:03 +0300

    updated_app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,10 +10,7 @@ import {Container,
 
 export class App extends Component {
   state = {
-    contacts: [    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [],
     filter: '',
   }
   
@@ -40,16 +37,21 @@ export class App extends Component {
         ({id}) => id!==idToDelete
       );
       return {contacts:updContactsArr};
+
     });
   }
 
   componentDidMount = () =>{
     const contact = JSON.parse(localStorage.getItem('contacts'));
-    this.setState({contact})
+    console.log(contact)
+    if (this.state.contacts === []) {
+      this.setState({contacts:[...contact]})
+    }
   }
 
-  componentDidUpdate = () =>{
-    localStorage.setItem('contacts',JSON.stringify(this.state.contacts))
+  componentDidUpdate = (prevProps,prevState) =>{
+    localStorage.setItem('contacts',JSON.stringify([...prevState.contacts]))
+
   }
 
 
